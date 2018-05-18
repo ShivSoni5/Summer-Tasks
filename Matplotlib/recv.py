706b505a174a2a5ffd0700661e43ba4fb2c33db3
ShivSoni5 <shivsonic05@gmail.com>
2018-05-18 10:31:00 +0530

    recursive call after 5 min remaining

--- Matplotlib/recv.py
+++ Matplotlib/recv.py
@@ -1,6 +1,7 @@
-#!/usr/bin/python2
+#!/usr/bin/python3
 
 import  socket,time
+import matplotlib.pyplot as plt
 
 rec_ip="127.0.0.1"
 myport=9999
@@ -11,17 +12,43 @@ s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 #  now connecting ip  and port 
 s.bind((rec_ip,myport)) 
 #  buffer size 
-c = []
+list_of_words = []
 
 timeout = time.time() + 10
 while time.time() < timeout:
-	a = s.recvfrom(1000)[0]
-	d = a.decode('utf-8')
-	b = d.split()
-	for i in range(len(b)):
-		c.append(b[i])	
-#		print(c)	
+	rec_data = s.recvfrom(1000)[0]
+	decoded_data = rec_data.decode('utf-8')
+	split_words = decoded_data.split()
+	for i in range(len(split_words)):
+		list_of_words.append(split_words[i])	
+#		print(list_of_words)	
 
-d=list(set(c))
-print(d)
-		
+set_of_words=list(set(list_of_words))
+#print(set_of_words)
+
+x={}
+
+for i in range(len(set_of_words)):
+	x[set_of_words[i]] = list_of_words.count(set_of_words[i])
+
+print(x)		
+
+x_axis_label = list(x.keys()) 
+
+x_axis_cor = list(range(1,len(x_axis_label)+1))
+
+y_axis_height = list(x.values())
+
+plt.bar(x_axis_cor, y_axis_height, tick_label = x_axis_label, width = 0.8)
+
+plt.title("data visualization")
+
+plt.xlabel('words --->')
+
+plt.ylabel('frequency -->')
+
+plt.show()
+
+
+
+	
--- Matplotlib/send.py
+++ Matplotlib/send.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python2
 
 import  socket 
+from time import sleep
 s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 
 a="hiiii this is my naeme"
 b=a.encode('utf-8')
 
-s.sendto(b,("127.0.0.1",9999))
+while True:
+	sleep(3)
+	s.sendto(b,("127.0.0.1",9999))
 
 
