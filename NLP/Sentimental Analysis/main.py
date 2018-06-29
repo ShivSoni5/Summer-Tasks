bd8a2ee1ca54cfa7eb290c6da377214f507b3d2a
ShivSoni5 <shivsonic05@gmail.com>
2018-06-30 00:25:53 +0530

    done

--- NLP/Sentimental Analysis/main.py
+++ NLP/Sentimental Analysis/main.py
@@ -3,7 +3,8 @@
 import tweepy
 import pandas as pd
 import numpy as np
-
+from textblob import TextBlob
+import re
 from IPython.display import display
 import matplotlib.pyplot as plt
 from credentials import *
@@ -17,7 +18,7 @@ def twitter_setup():
 
 extractor = twitter_setup()
 
-tweets = extractor.user_timeline(screen_name = "narendramodi", count = 20)
+tweets = extractor.user_timeline(screen_name = "realDonaldTrump", count = 20)
 '''
 print(f"Number of tweets extracted: {len(tweets)}.\n")
 print("5 recent tweets:\n")
@@ -36,6 +37,33 @@ data['Likes'] = np.array([tweet.favorite_count for tweet in tweets])
 data['RTs'] = np.array([tweet.retweet_count for tweet in tweets])
 
 # we display the first 10 elements of the dataframe:
+#display(data.head(10))
+
+def clean_tweet(tweet):
+    return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
+
+def analize_sentiment(tweet):
+    analysis = TextBlob(clean_tweet(tweet))
+    if analysis.sentiment.polarity > 0:
+        return 1
+    elif analysis.sentiment.polarity == 0:
+        return 0
+    else:
+        return -1
+
+data['SA'] = np.array([analize_sentiment(tweet) for tweet in data['Tweets']])
+
 display(data.head(10))
 
+# we construct lists with classified tweets:
+
+pos_tweets = [tweet for index, tweet in enumerate(data['Tweets']) if data['SA'][index] > 0 ]
+neu_tweets = [tweet for index, tweet in enumerate(data['Tweets']) if data['SA'][index] == 0 ]
+neg_tweets = [tweet for index, tweet in enumerate(data['Tweets']) if data['SA'][index] < 0 ]
+
+# we print percentages:
+
+print (f"Percentage of positive tweets: {len(pos_tweets)*100/len(data['Tweets'])}%")
+print (f"Percentage of neutral tweets: {len(neu_tweets)*100/len(data['Tweets'])}%")
+print (f"Percentage of negative tweets: {len(neg_tweets)*100/len(data['Tweets'])}%")
 
