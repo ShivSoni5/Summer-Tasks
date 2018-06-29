624265c9d9bd4540cc0dda1917b791e884dd99ae
ShivSoni5 <shivsonic05@gmail.com>
2018-06-29 23:51:21 +0530

    NaMo

--- NLP/Sentimental Analysis/main.py
+++ NLP/Sentimental Analysis/main.py
@@ -17,9 +17,25 @@ def twitter_setup():
 
 extractor = twitter_setup()
 
-tweets = extractor.user_timeline(screen_name = "maddhruv", count = 20)
+tweets = extractor.user_timeline(screen_name = "narendramodi", count = 20)
+'''
 print(f"Number of tweets extracted: {len(tweets)}.\n")
 print("5 recent tweets:\n")
 for tweet in tweets[:5]:
     print(tweet.text)
     print()
+'''
+# we create a pandas dataframe as follows:
+data = pd.DataFrame(data=[tweet.text for tweet in tweets], columns=['Tweets'])
+
+# we add relevent data:
+data['len'] = np.array([len(tweet.text) for tweet in tweets])
+data['Date'] = np.array([tweet.created_at for tweet in tweets])
+data['Source'] = np.array([tweet.source for tweet in tweets])
+data['Likes'] = np.array([tweet.favorite_count for tweet in tweets])
+data['RTs'] = np.array([tweet.retweet_count for tweet in tweets])
+
+# we display the first 10 elements of the dataframe:
+display(data.head(10))
+
+
