534e57f6d9492aa856dfab625763003304d033b0
ShivSoni5 <shivsonic05@gmail.com>
2018-06-29 16:43:25 +0530

    how to remove punctuation and stop words

--- NLP/stopword.py
+++ NLP/stopword.py
@@ -2,13 +2,15 @@
 
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
+from string import punctuation
 
 #Tokenizing
 sentence = "Jim is bringing his bulldog to eat at Friendlys?"
 tokens = word_tokenize(sentence)
 print (tokens)
 
-#Removing Stop-words like 'is','his',etc.
+#Removing Stop-words like 'is','his' and pucntuations.
 stop_words = set(stopwords.words('english'))
-tokens = [w for w in tokens if w not in stop_words]
+all_stop_words = stop_words | set(punctuation)  # bitwise OR in stop words and punctuation marks
+tokens = [w for w in tokens if w not in all_stop_words]
 print (tokens)
