9c053580d76780648bfc071e249c57076ae81fc5
ShivSoni5 <shivsonic05@gmail.com>
2018-09-19 09:26:43 +0530

    storing data in dbms

--- Pandas/read_excel/mini_project/main.py
+++ Pandas/read_excel/mini_project/main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template
+import mysql.connector as mysql
 
 app = Flask(__name__)
 
@@ -7,9 +8,17 @@ def index():
     if request.method == 'POST':
         first_name = request.form['firstname']
         last_name = request.form['lastname']
-        print(first_name, last_name)
+        age = int(request.form['age'])
+#        print(first_name, last_name, age)
+        mariadb = mysql.connect(user='raj',password='redhat',database='PandaProject')
+        cursor = mariadb.cursor()
+        query = (f'insert into userdata(First_Name, Second_Name, Age) values("{first_name}","{last_name}",{age});')
+        cursor.execute(query)
+        mariadb.commit()
+        mariadb.close()
+
     return render_template('get_data.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
