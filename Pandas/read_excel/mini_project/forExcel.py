1d0d05ca6b1b62c5e7c34ca13e09d287dea62936
ShivSoni5 <shivsonic05@gmail.com>
2018-09-20 12:43:06 +0530

    done

--- Pandas/read_excel/mini_project/forExcel.py
+++ Pandas/read_excel/mini_project/forExcel.py
@@ -1,7 +1,8 @@
 from flask import Flask, request, render_template
 import pandas as pd
+import os
+import xlsxwriter
 
-data = pd.DataFrame(columns=['First Name','Second Name','Age'])
 app = Flask(__name__)
 
 @app.route('/', methods=['GET','POST'])
@@ -9,21 +10,31 @@ def index():
     if request.method == 'POST':
         first_name = request.form['firstname']
         last_name = request.form['lastname']
-        age = int(request.form['age'])
-        writer = pd.ExcelWriter('first.xlsx', engine='xlsxwriter')
-        global data
-        data1 = pd.DataFrame([first_name],columns=['First Name'])
-        data2 = pd.DataFrame([last_name],columns=['Second Name'])
-        data3 = pd.DataFrame([age],columns=['Age'])
-        data = data.append([[data1],[data2],[data3]])
-#        data = data.append(data1)
-#        data = data.append(data2)
-#        data = data.append(data3)
-        data.to_excel(writer, sheet_name = 'Sheet1')
-        writer.save()
+        age = request.form['age']
+
+        if os.path.exists('pandas.xlsx') is False:
+            create_file()
+            insert_data(first_name, last_name, age)
+
+        else:
+            insert_data(first_name, last_name, age)
+
 
     return render_template('get_data.html')
 
+def create_file():
+    workbook = xlsxwriter.Workbook('pandas.xlsx')
+    worksheet = workbook.add_worksheet()
+    workbook.close()
+
+def insert_data(FirstName, LastName, Age):
+    out = pd.read_excel('pandas.xlsx')
+    df = pd.DataFrame({'First Name': [FirstName], 'Last Name': [LastName], 'Age': [Age]})
+    append_data = pd.concat([out,df]) 
+
+    writer = pd.ExcelWriter('pandas.xlsx', engine='xlsxwriter')
+    append_data.to_excel(writer, sheet_name='Sheet1', index=0)
+    writer.save()
 
 if __name__ == '__main__':
     app.run(debug=True)
