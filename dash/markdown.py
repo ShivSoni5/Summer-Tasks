5ae65a16fb79663ea5a54260fd52003288d3cbec
ShivSoni5 <shivsonic05@gmail.com>
2018-09-21 14:41:12 +0530

    write markdown

---
+++ dash/markdown.py
@@ -0,0 +1,24 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+app = dash.Dash()
+
+markdown_text = '''
+### Dash and Markdown
+
+Dash apps can be written in Markdown.
+Dash uses the [CommonMark](http://commonmark.org/)
+specification of Markdown.
+Check out their [60 Second Markdown Tutorial](http://commonmark.org/help/)
+if this is your first introduction to Markdown!
+'''
+
+app.layout = html.Div([
+    dcc.Markdown(children=markdown_text)
+])
+
+app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
+
+if __name__ == '__main__':
+    app.run_server()
