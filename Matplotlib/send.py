322de65adfe32c42e3a4c88116ae2441d2dc99f1
ShivSoni5 <shivsonic05@gmail.com>
2018-09-21 10:46:37 +0530

    send multiple messages

--- Matplotlib/send.py
+++ Matplotlib/send.py
@@ -4,11 +4,11 @@ import  socket
 from time import sleep
 s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 
-a="hiiii this is my naeme"
-b=a.encode('utf-8')
 
 while True:
-	sleep(2)
-	s.sendto(b,("127.0.0.1",9999))
+        a = input()
+        b = a.encode('utf-8')
+        s.sendto(b,("127.0.0.1",9999))
+        sleep(2)
 
 
