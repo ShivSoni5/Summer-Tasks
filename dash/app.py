b86d3ac2f3594ff9c4e9401edd5c5dc9c51eb830
ShivSoni5 <shivsonic05@gmail.com>
2018-09-25 02:57:00 +0530

    changes i dont know

--- dash/app.py
+++ dash/app.py
@@ -28,7 +28,7 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
             figure={
                 'data': [
                     {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+                    {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montre    al'},
             ],
             'layout': {
                 'plot_bgcolor': colors['background'],
--- numpy_task4.py
+++ numpy_task4.py
@@ -1,26 +1,21 @@
 #!/usr/bin/python3
-
 import numpy as np
 
 list_for_array = []
-
 
 while True:
 	a=input("enter number for array: ")
 	if a == "q":
 		break
 	list_for_array.append(int(a))
-	
 
 length_list = len(list_for_array)
 array_shapes = []
-
 
 def generate_shapes(list_for_array_shapes,length):
 	for i in range(2, length//2 + 1):
 		if (length % i) == 0:
 			list_for_array_shapes.append((i,length//i))
-
 
 generate_shapes(array_shapes,length_list)
 
